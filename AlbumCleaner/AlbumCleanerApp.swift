5050bb047b77a4520ecc46e7ecf04793b235c939
Tomopu <m.poochian@gmail.com>
2025-10-22 22:23:49 +0900

    Initial Commit

---
+++ AlbumCleaner/AlbumCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumCleanerApp.swift
+//  AlbumCleaner
+//
+//  Created by 泉知成 on 2025/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AlbumCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlbumCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlbumCleaner
+//
+//  Created by 泉知成 on 2025/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
